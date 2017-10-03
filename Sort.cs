98a2da045c1f0461d60e2a8d3c2046886928eeba
Mikhail Gerdov <Mikhail.Gerdov@igt.com>
2017-10-03 11:35:33 -0300

    initial commit

---
+++ Sort.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mgerdov
+ * Date: 10/3/2017
+ * Time: 10:12 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace TaskManager
+{
+	/// <summary>
+	/// Description of Sort.
+	/// </summary>
+	public class Sort
+	{
+	    public string Property { get; set; }
+	    public string Direction { get; set; }
+		
+
+	    public static Sort[] LoadSorts()
+	    {
+	    	List<Sort> sorts = new List<Sort>();
+			
+			XmlDocument xDoc1 = new XmlDocument();
+			xDoc1.Load(Program.DataDir + "Sorting.xml");
+	        // получим корневой элемент
+	        XmlElement xRoot1 = xDoc1.DocumentElement;
+	        
+	        // обход всех узлов в корневом элементе
+	        foreach(XmlNode xnode in xRoot1)
+	        {
+	        	Sort sort = new Sort();
+	        	
+	        	// получаем атрибут name
+/*	            if(xnode.Attributes.Count>0)
+	            {
+	                XmlNode attr = xnode.Attributes.GetNamedItem("id");
+	                if (attr!=null)
+	                    Console.WriteLine(attr.Value);
+	            }
+*/	            // обходим все дочерние узлы элемента user
+	            foreach(XmlNode childnode in xnode.ChildNodes)
+	            {
+	            	// если узел - property
+	                if(childnode.Name == "property")
+	                {
+	                    Console.WriteLine("Property: {0}", childnode.InnerText);
+	                    sort.Property = childnode.InnerText;
+	                }
+	            	
+	                // если узел - direction
+	                if(childnode.Name == "direction")
+	                {
+	                    Console.WriteLine("Направление: {0}", childnode.InnerText);
+	                    sort.Direction = childnode.InnerText;
+	                }
+	            }
+	            sorts.Add(sort);
+	        }
+	        return sorts.ToArray();
+	    }
+	    
+		public static IEnumerable<Task> DoSort<Task>(IEnumerable<Task> tasks, Sort[] sort)
+		{
+		    IOrderedEnumerable<Task> temp = null;
+		    foreach (var s in sort)
+		    {
+		        Func<Task, IComparable> keySelector = GetKeySelector<Task>(s.Property);
+		
+		        if (temp == null)
+		        {
+		            temp = s.Direction == "Asc" ?
+		                    tasks.OrderBy(keySelector) :
+		                    tasks.OrderByDescending(keySelector);
+		        }
+		        else
+		        {
+		            temp = s.Direction == "Asc" ?
+		                    temp.ThenBy(keySelector) :
+		                    temp.ThenByDescending(keySelector);
+		        }
+		    }
+		
+		    return temp ?? tasks;
+		}
+		
+		private static Func<Task, IComparable> GetKeySelector<Task>(string property)
+		{
+		    var param = Expression.Parameter(typeof(Task));
+		    var lambda = Expression.Lambda<Func<Task, IComparable>>(
+		        Expression.Convert(
+		            Expression.Property(param, property),
+		            typeof(IComparable)),
+		        param);
+		    return lambda.Compile();
+		}
+		
+	}
+}
---
+++ Task.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mgerdov
+ * Date: 10/3/2017
+ * Time: 10:11 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace TaskManager
+{
+	/// <summary>
+	/// Description of Task.
+	/// </summary>
+	public class Task
+	{
+		public int id { get; set; }
+		public string title { get; set; }
+		public int weight { get; set; }
+		public int parent { get; set; }
+
+		public static List<Task> LoadTasks()
+		{
+			List <Task> tasks = new List <Task>();
+			
+			XmlDocument xDoc0 = new XmlDocument();
+			xDoc0.Load(Program.DataDir + "Tasks.xml");
+	        // получим корневой элемент
+	        XmlElement xRoot0 = xDoc0.DocumentElement;
+	        // обход всех узлов в корневом элементе
+	        foreach(XmlNode xnode in xRoot0)
+	        {
+	        	Task task = new Task();
+	        	// получаем атрибут name
+/*	            if(xnode.Attributes.Count>0)
+	            {
+	                XmlNode attr = xnode.Attributes.GetNamedItem("id");
+	                if (attr!=null)
+	                    Console.WriteLine(attr.Value);
+	            }
+*/	            // обходим все дочерние узлы элемента user
+	            foreach(XmlNode childnode in xnode.ChildNodes)
+	            {
+	            	// если узел - id
+	                if(childnode.Name == "id")
+	                {
+	                    Console.WriteLine("ID: {0}", childnode.InnerText);
+	                    task.id = Int32.Parse(childnode.InnerText);
+	                }
+	            	
+	                // если узел - title
+	                if(childnode.Name == "title")
+	                {
+	                    Console.WriteLine("Название: {0}", childnode.InnerText);
+	                    task.title = childnode.InnerText;
+	                }
+	                
+	                // если узел - weight
+	                if (childnode.Name == "weight")
+	                {
+	                    Console.WriteLine("Вес: {0}", childnode.InnerText);
+	                    task.weight = Int32.Parse(childnode.InnerText);
+	                }
+	                
+	                // если узел - parent
+	                if(childnode.Name == "parent")
+	                {
+	                    Console.WriteLine("Parent: {0}", childnode.InnerText);
+	                    task.parent = Int32.Parse(childnode.InnerText);
+	                }
+	            }
+	            tasks.Add(task);
+	        }
+	        return tasks;
+		}
+	
+		public static void AddTask(List <Task> tasks, int id, string title, int weight, int parent)
+		{
+			// add to collection
+			Task task = new Task();
+			task.id = id;
+			task.title = title;
+			task.weight = weight;
+			task.parent = parent;
+			tasks.Add(task);
+			Console.WriteLine("Adding..."  + task);
+
+			// add to XML
+			XmlDocument xDoc = new XmlDocument();
+			xDoc.Load(Program.DataDir + "Tasks.xml");
+			XmlElement xRoot = xDoc.DocumentElement;
+			
+			XmlElement NewTask = xDoc.CreateElement("task");
+			
+			// Создаём элементы
+			XmlElement ElemId = xDoc.CreateElement("id");
+			XmlElement ElemTitle = xDoc.CreateElement("title");
+			XmlElement ElemWeight = xDoc.CreateElement("weight");
+			XmlElement ElemParent = xDoc.CreateElement("parent");
+			
+			// Создаём текстовые значения
+			XmlText IdText = xDoc.CreateTextNode(id.ToString());
+			XmlText TitleText = xDoc.CreateTextNode(title);
+			XmlText WeightText = xDoc.CreateTextNode(weight.ToString());
+			XmlText ParentText = xDoc.CreateTextNode(parent.ToString());
+			
+			// Добавляем узлы
+			ElemId.AppendChild(IdText);
+			ElemTitle.AppendChild(TitleText);
+			ElemWeight.AppendChild(WeightText);
+			ElemParent.AppendChild(ParentText);
+			
+			// Добавляем элементы
+			NewTask.AppendChild(ElemId);
+			NewTask.AppendChild(ElemTitle);
+			NewTask.AppendChild(ElemWeight);
+			NewTask.AppendChild(ElemParent);
+			
+			// Добавляем в корень
+			xRoot.AppendChild(NewTask);
+			
+			xDoc.Save("Tasks-mod.xml");
+		}
+		
+		public static void ChangeTask(List <Task> tasks, int id, string whichParam, object newValue)
+		{
+			// replace in collection
+			Task t = FindTaskById(tasks, id);
+			if(t == null)
+			{
+				Console.WriteLine("Not found");
+				return;		// no such task
+			}
+			t.GetType().GetProperty(whichParam).SetValue(t, newValue, null);	//TODO: validate reflection result
+			
+			// replace in XML
+			XDocument xDoc = XDocument.Load(Program.DataDir + "Tasks.xml");
+			XElement root = xDoc.Element("tasks");
+			foreach (XElement e in root.Elements("task").ToList())
+			{
+				if (e.Element("id").Value == id.ToString())
+				{
+					Console.WriteLine("Found: " + e.Element(whichParam).Value);
+					e.Element(whichParam).Value = newValue.ToString();
+					Console.WriteLine("Replaced with: " + e.Element(whichParam).Value);
+				}
+				else Console.WriteLine("Not found");
+			}
+			xDoc.Save(Program.DataDir + "Tasks-adj.xml");
+		}
+		
+		public static Task FindTaskById(List <Task> tasks, int id)
+		{
+			foreach(Task t in tasks)
+			{
+				if(t.id == id)
+					return t;
+			}
+			return null;
+		}
+		
+	}
+}
